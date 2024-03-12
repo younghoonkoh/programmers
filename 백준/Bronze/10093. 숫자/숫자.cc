7bf04cb6ff72b74267745eeb633cd0254073dbb4
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-03-12 21:30:17 +0900

    [Bronze II] Title: 숫자, Time: 20 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -12,21 +12,21 @@ int main()
     ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
     cin.tie(NULL); // 입력과 출력 연결을 끊어주기
 
-    int A, B;
+    long long A, B;
 
     cin >> A >> B;
 
-    if (A > B) {
+    if (A > B+1) {
         cout << A - B - 1 << "\n";
 
-        for (int i = B + 1; i < A; ++i) {
+        for (long long i = B + 1; i < A; ++i) {
             cout << i << " ";
         }
     }
 
-    else if (A < B) {
+    else if (A +1< B) {
         cout << B - A - 1 << "\n";
-        for (int i = A + 1; i < B; ++i) {
+        for (long long i = A + 1; i < B; ++i) {
             cout << i << " ";
         }
     }
