406193e6de35e8e6d741acc0b55e2b741ddab6cf
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-03-15 17:51:26 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -2,37 +2,41 @@
 #include <string>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+
+
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
+    cin.tie(NULL); // 입력과 출력 연결을 끊어주기
+
+
 
     string s;
     cin >> s;
-
-    int number[10] = {};
     int sum = 0;
 
-    // 각 숫자의 개수 세기
+    int number[10] = {};
+
     for (int i = 0; i < s.size(); ++i) {
         number[s[i] - '0']++;
     }
 
-    // 6과 9 처리
-    int sixNineCount = (number[6] + number[9] + 1) / 2;
-    number[6] = sixNineCount;
-    number[9] = sixNineCount;
 
-    // 가장 큰 값 찾기
     for (int i = 0; i < 10; ++i) {
-        if (i != 6 && i != 9 && number[i] > sum) {
-            sum = number[i];
+
+        if (i != 9 && i != 6) {
+            if (number[i] > sum) {
+                sum = number[i];
+            }
         }
     }
 
-    // 최종 필요한 세트의 개수 계산
-    int result = (sum > sixNineCount) ? sum : sixNineCount;
-    
-    cout << result;
+    if (sum < (number[6] + number[9] + 1) / 2) {
+        sum = (number[6] + number[9] + 1) / 2;
+    }
+    cout << sum;
 
     return 0;
 }
