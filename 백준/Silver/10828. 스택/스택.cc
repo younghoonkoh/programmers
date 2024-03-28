1727b27e1ee38451a209aa0aa1aefecc266cd179
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-03-28 18:12:07 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,51 +1,9 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
-const int mx = 100000;
-int arr[mx];
-int pos = 0;
-    
-void push(int N) {
 
-
-
-    arr[pos++] = N;
-
-}
-
-int pop() {
-    if (arr[pos-1] == NULL) {
-        return - 1;
-    }
-
-    pos--;
-    return arr[pos];
-   
-
-}
-
-int size() {
-    return pos;
-}
-
-int empty() {
-    if (arr[pos - 1] == NULL) {
-        return 1;
-    }
-    else return 0;
-}
-
-
-int top() {
-
-    if (arr[pos - 1] == NULL) {
-        return - 1;
-    }
-
-    return arr[pos - 1];
-
-}
     
 
 
@@ -54,37 +12,46 @@ int main(){
    
     ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
     cin.tie(NULL); // 입력과 출력 연결을 끊어주기
-    int N = 0;
-    cin >> N;
-    string s = "";
-    for (int i = 0; i < N; ++i) {
-        cin >> s;
-        
-        if (s == "push") {
-            int N;
-            cin >> N;
-            push(N);
-        }
-        
-        if (s == "pop") {
-            cout << pop() << '\n';
+
+    int n;
+    cin >> n;
+    stack<int> s;
+
+    while (n--) {
+        string c;
+        cin >> c;
+        if (c == "push") {
+            int t;
+            cin >> t;
+            s.push(t);
         }
 
-        if (s == "size") {
-            cout <<size() << '\n';
+        else if (c == "pop") {
+            if (s.empty()) {
+                cout << -1 << "\n";
+            }
+            else {
+                cout << s.top() << "\n";
+                s.pop();
+            }
         }
 
-        if (s == "empty") {
-            cout << empty() << '\n';
+        else if (c == "size") {
+            cout << s.size() << "\n";
         }
 
-        if (s == "top") {
-            cout << top() << '\n';
+        else if (c == "empty") {
+            cout << (int)s.empty() << "\n";
+        }
+
+        else {
+            if (s.empty()) {
+                cout << -1 << "\n";
+            }
+            else cout << s.top() << "\n";
         }
 
     }
-   
-
 
   
     return 0;
