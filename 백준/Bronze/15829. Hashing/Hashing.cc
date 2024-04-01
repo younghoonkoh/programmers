8e293d9c181aa7dfcd0951d912a7a5d929c88e63
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-04-01 21:02:29 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+
+
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
+    cin.tie(NULL); // 입력과 출력 연결을 끊어주기
+
+    int N;
+    cin >> N;
+
+    string a;
+
+    long long sum = 0;
+
+    int alpha[27] = {};
+
+    const long long M = 1234567891;
+
+    long long r = 1;
+    cin >> a;
+    int b[27] = {};
+    for (int i = 0; i < N; ++i) {
+       
+        b[i] = a[i] - 'a' +1;
+        sum += b[i] * r % M;
+        r = r * 31 % M;
+    }
+        
+
+    cout << sum;
+
+    return 0;
+}
