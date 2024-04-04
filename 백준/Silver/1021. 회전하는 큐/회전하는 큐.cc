db9abc79eac8abf2c973421e0f8c93c7af44e5c8
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-04-04 22:06:08 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <deque>
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
+    cin.tie(NULL); // 입력과 출력 연결을 끊어주기
+
+    int N, M;
+
+    cin >> N>>M;
+
+    deque<int> s;
+    int idx= 0;
+    int res = 0;
+    int c = 0;
+
+    for (int i = 1; i <= N; ++i) {
+        s.push_back(i);
+    }
+
+    while (M--) {
+        cin >> c;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == c) {
+                idx = i;
+                break;                      // 숫자를 뽑아내면 for문 탈출
+            }
+        }
+        
+        //앞
+        if (idx <= s.size() / 2) {
+            while (1) {
+                if (s.front() == c) {
+                    s.pop_front();
+                    break;
+                }
+                ++res;
+                s.push_back(s.front());
+                s.pop_front();
+            }
+            
+        }
+        //뒤
+        else {
+            while (1) {
+                if(s.front() == c){
+                    s.pop_front();
+                    break;
+                }
+                ++res;
+                s.push_front(s.back());
+                s.pop_back();
+            }
+
+        }
+
+    }
+    cout << res;
+
+
+
+
+   
+    return 0;
+} 
