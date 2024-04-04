e3a879dd428cc7e3ed7363976cb8b26d7e6ea9a4
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-04-04 16:52:38 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 
 using namespace std;
 
@@ -14,45 +14,28 @@ int main() {
     cin.tie(NULL); // 입력과 출력 연결을 끊어주기
 
 
-    while (true)
-    {
-        string N = "";
-        
+    string N;
+    
+    while (true) {
         cin >> N;
-        int a = 0, b = 0;
+        string b = N;
+        reverse(N.begin(), N.end());
 
         if (N == "0") {
             break;
         }
 
-        if (N.size() % 2 == 1) {
-            for (int i = 0; i < N.size() / 2; ++i) {
-                if (N[i] == N[N.size() - 1 - i]) {
-                    ++a;
-                }
-            }
-            if (a == N.size() / 2) {
-                cout << "yes" << "\n";
-            }
-            else cout << "no" << "\n";
-
+        else if (N == b) {
+            cout << "yes\n";
+        }
+        else {
+            cout << "no\n";
         }
 
-        else if (N.size() % 2 == 0) {
-            for (int i = 0; i < N.size() / 2; ++i) {
-                if (N[i] == N[N.size() - i -1]) {
-                    ++b;
-                }              
-            }
-            if (b == N.size() / 2) {
-                cout << "yes" << "\n";
-            }
-            else cout << "no" << "\n";
-        }
-        
-        
-        
     }
+        
+        
+    
 
 
 
