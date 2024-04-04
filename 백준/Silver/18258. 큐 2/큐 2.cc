76832fe18916685d31ad3e7ab8013bc7c745a209
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-04-04 18:28:40 +0900

    [Silver IV] Title: 큐 2, Time: 444 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
+    cin.tie(NULL); // 입력과 출력 연결을 끊어주기
+
+    long long n;
+    cin >> n;
+
+    queue<int> s;
+
+    while (n--) {
+        string c;
+        cin >> c;
+
+        if (c == "push") {
+            int t;
+            cin >> t;
+
+            s.push(t);
+        }
+        if (c == "pop") {
+            if (s.empty()) {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << s.front() << "\n";
+                s.pop();
+            }
+
+        }
+        if (c == "size") {
+            cout << s.size() << "\n";
+        }
+
+        if (c == "empty") {
+            cout << s.empty() << "\n";
+        }
+
+        if (c == "front") {
+            if (s.empty()) {
+                cout << -1 << "\n";
+            }
+            else cout << s.front() << "\n";
+        }
+
+        if (c == "back") {
+            if (s.empty()) {
+                cout << -1 << "\n";
+            }
+            else cout << s.back() << "\n";
+        }
+    }
+    return 0;
+} 
