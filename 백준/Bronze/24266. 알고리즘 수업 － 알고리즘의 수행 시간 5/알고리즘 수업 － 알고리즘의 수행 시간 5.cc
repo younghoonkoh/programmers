0c17f90683822c626b80373c9b7c1ecd1419bc1b
younghoonkoh <115515764+younghoonkoh@users.noreply.github.com>
2024-04-09 17:10:11 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+
+    ios_base::sync_with_stdio(false); // C와 C++ 스트림의 동기화 해제
+    cin.tie(NULL); // 입력과 출력 연결을 끊어주기
+
+
+    long long n;
+
+    cin >> n;
+
+    cout << n*n*n << "\n" << 3;
+
+
+
+    
+       
+
+    
+   
+    return 0;
+} 
